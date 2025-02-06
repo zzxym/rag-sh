b783f98d4c741fdfbdc1e2160adcd3a121488cf6
Zihao Zhang <zhangzihaopk@gmail.com>
2025-02-06 19:24:43 +0800

    增加0.15 相关脚本

--- dify/0.15.2/images.sh
+++ dify/0.15.2/images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo 'dify 0.15.1 拉取镜像开始'
+echo 'dify 0.15.2 拉取镜像开始'
 
-docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_api:0.15.1
-docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_web:0.15.1
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_api:0.15.2
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_web:0.15.2
 docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/redis:6-alpine
 docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/postgres:15-alpine
 docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/nginx:latest
@@ -11,14 +11,14 @@ docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/d
 docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ubuntu_squid:latest
 docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/semitechnologies_weaviate:1.19.0
 
-echo 'dify 0.15.1 拉取镜像完成'
+echo 'dify 0.15.2 拉取镜像完成'
 
 sleep 2
 
-echo 'dify 0.15.1 镜像tag'
+echo 'dify 0.15.2 镜像tag'
 
-docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_api:0.15.1 langgenius/dify-api:0.15.1
-docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_web:0.15.1 langgenius/dify-web:0.15.1
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_api:0.15.2 langgenius/dify-api:0.15.2
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/dify_web:0.15.2 langgenius/dify-web:0.15.2
 docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/redis:6-alpine redis:6-alpine
 docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/postgres:15-alpine postgres:15-alpine
 docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/nginx:latest nginx:latest
@@ -26,4 +26,4 @@ docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/di
 docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ubuntu_squid:latest ubuntu/squid:latest
 docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/semitechnologies_weaviate:1.19.0 semitechnologies/weaviate:1.19.0
 
-echo 'dify 0.15.1 镜像tag完成'
+echo 'dify 0.15.2 镜像tag完成'
---
+++ ragflow/0.15.1/images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo 'ragflow v0.15.1 拉取镜像开始'
+
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ragflow:v0.15.1
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ragflow_edwardelric233:oc9
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/valkey:8
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/mysql:8.0.39
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/quay.io_minio:RELEASE.2023-12-20T01-00-02Z
+
+echo 'ragflow v0.15.1 拉取镜像完成'
+
+sleep 2
+
+echo 'ragflow v0.15.1 镜像tag'
+
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ragflow:v0.15.1 infiniflow/ragflow:v0.15.1
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ragflow_edwardelric233:oc9 edwardelric233/ragflow:oc9
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/valkey:8 valkey/valkey:8
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/mysql:8.0.39 mysql:8.0.39
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/quay.io_minio:RELEASE.2023-12-20T01-00-02Z quay.io/minio/minio:RELEASE.2023-12-20T01-00-02Z
+
+echo 'ragflow v0.15.1 镜像tag完成'
