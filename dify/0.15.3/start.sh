3964f47ea0f40bf872b9630c4ac46a2a4d237873
Zihao Zhang <zhangzihaopk@gmail.com>
2025-02-25 19:31:49 +0800

    修改脚本

--- dify/0.15.3/start.sh
+++ dify/0.15.3/start.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-echo 'dify 0.15.3 开始安装'
+# 检查是否安装了 Docker
+if ! command -v docker &> /dev/null; then
+    echo "Docker 未安装，请先安装 Docker。"
+    exit 1
+fi
 
+echo "Docker 已安装。"
+docker_version=$(docker --version)
+echo "Docker 版本: $docker_version"
+
+# 检查是否安装了 Docker Compose
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose 未安装，请先安装 Docker Compose。"
+    exit 1
+fi
+
+echo "Docker Compose 已安装。"
+compose_version=$(docker-compose --version)
+echo "Docker Compose 版本: $compose_version"
+
+
+echo 'dify 0.15.3 开始安装'
 sleep 1
 
 mkdir -p /data/yunxinai/dify/0.15.3
@@ -13,7 +33,24 @@ sleep 1
 yum install -y git
 echo 'dify 0.15.3 安装git完成'
 
-git clone https://github.com/langgenius/dify.git
+git clone https://gitcode.com/yunxinai/rag-sh.git
+
+echo 'dify 0.15.3 克隆git完成'
+
+sh rag-sh/dify/0.15.3/images.sh
+
+echo 'dify 0.15.3 拉取镜像完成'
+
+cd /data/yunxinai/dify/0.15.3
+
+git clone https://gitcode.com/yunxinai/rag-file.git
+
+cd dify/0.15.3/dify
+
+docker-compose up -d
+
+echo 'dify 0.15.3 安装完成'
+
 
 
 
