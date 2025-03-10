de5a7732a11e4f80c599617eb5eb11b50411352d
Zihao Zhang <zhangzihaopk@gmail.com>
2025-03-10 19:09:36 +0800

    修改ragflow脚本

--- ragflow/0.17.0/images.sh
+++ ragflow/0.17.0/images.sh
@@ -1,57 +1,23 @@
 #!/bin/bash
 
-# 检查是否安装了 Docker
-if ! command -v docker &> /dev/null; then
-echo "Docker 未安装，请先安装 Docker。"
-exit 1
-fi
+echo 'ragflow v0.17.0 拉取镜像开始'
 
-echo "Docker 已安装。"
-docker_version=$(docker --version)
-echo "Docker 版本: $docker_version"
-
-        # 检查是否安装了 Docker Compose
-if ! command -v docker-compose &> /dev/null; then
-echo "Docker Compose 未安装，请先安装 Docker Compose。"
-exit 1
-fi
-
-echo "Docker Compose 已安装。"
-compose_version=$(docker-compose --version)
-echo "Docker Compose 版本: $compose_version"
-
-
-echo 'ragflow v0.17.0 开始安装'
-sleep 1
-
-mkdir -p /data/yunxinai/ragflow/0.17.0
-cd /data/yunxinai/ragflow/0.17.0
-
-echo 'ragflow v0.17.0 创建目录完成'
-sleep 1
-
-yum install -y git
-echo 'ragflow v0.17.0 安装git完成'
-
-cd /data/yunxinai/ragflow/0.17.0
-git clone https://gitcode.com/yunxinai/rag-sh.git
-
-echo 'ragflow v0.17.0 克隆git完成'
-
-sh /data/yunxinai/ragflow/0.17.0/rag-sh/ragflow/0.17.0/images.sh
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ragflow:v0.17.0
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/valkey:8
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/mysql:8.0.39
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/quay.io_minio:RELEASE.2023-12-20T01-00-02Z
+docker pull crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/elasticsearch:8.11.3
 
 echo 'ragflow v0.17.0 拉取镜像完成'
 
-cd /data/yunxinai/ragflow/0.17.0
+sleep 2
 
-git clone https://gitcode.com/yunxinai/rag-file.git
+echo 'ragflow v0.17.0 镜像tag'
 
-cd /data/yunxinai/ragflow/0.17.0/rag-file/ragflow/0.17.0/ragflow/docker
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/ragflow:v0.17.0 infiniflow/ragflow:v0.17.0
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/valkey:8 valkey/valkey:8
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/mysql:8.0.39 mysql:8.0.39
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/quay.io_minio:RELEASE.2023-12-20T01-00-02Z quay.io/minio/minio:RELEASE.2023-12-20T01-00-02Z
+docker tag crpi-33mr80vehc50lqh8.cn-chengdu.personal.cr.aliyuncs.com/yunxinai/elasticsearch:8.11.3 elasticsearch:8.11.3
 
-docker-compose up -d
-
-echo 'ragflow v0.17.0 安装完成'
-
-
-
-
+echo 'ragflow v0.17.0 镜像tag完成'
--- ragflow/0.17.0/start.sh
+++ ragflow/0.17.0/start.sh
@@ -2,18 +2,18 @@
 
 # 检查是否安装了 Docker
 if ! command -v docker &> /dev/null; then
-    echo "Docker 未安装，请先安装 Docker。"
-    exit 1
+echo "Docker 未安装，请先安装 Docker。"
+exit 1
 fi
 
 echo "Docker 已安装。"
 docker_version=$(docker --version)
 echo "Docker 版本: $docker_version"
 
-# 检查是否安装了 Docker Compose
+        # 检查是否安装了 Docker Compose
 if ! command -v docker-compose &> /dev/null; then
-    echo "Docker Compose 未安装，请先安装 Docker Compose。"
-    exit 1
+echo "Docker Compose 未安装，请先安装 Docker Compose。"
+exit 1
 fi
 
 echo "Docker Compose 已安装。"
@@ -21,38 +21,36 @@ compose_version=$(docker-compose --version)
 echo "Docker Compose 版本: $compose_version"
 
 
-echo 'dify 1.0.0 开始安装'
+echo 'ragflow 0.17.0 开始安装'
 sleep 1
 
-mkdir -p /data/yunxinai/dify/1.0.0
-cd /data/yunxinai/dify/1.0.0
+mkdir -p /data/yunxinai/ragflow/0.17.0
+cd /data/yunxinai/ragflow/0.17.0
 
-echo 'dify 1.0.0 创建目录完成'
+echo 'ragflow 0.17.0 创建目录完成'
 sleep 1
 
 yum install -y git
-echo 'dify 1.0.0 安装git完成'
+echo 'ragflow 0.17.0 安装git完成'
 
-cd /data/yunxinai/dify/1.0.0
+cd /data/yunxinai/ragflow/0.17.0
 git clone https://gitcode.com/yunxinai/rag-sh.git
 
-echo 'dify 1.0.0 克隆git完成'
+echo 'ragflow 0.17.0 克隆git完成'
 
-sh /data/yunxinai/dify/1.0.0/rag-sh/dify/1.0.0/images.sh
+sh /data/yunxinai/ragflow/0.17.0/rag-sh/ragflow/0.17.0/images.sh
 
-echo 'dify 1.0.0 拉取镜像完成'
+echo 'ragflow 0.17.0 拉取镜像完成'
 
-cd /data/yunxinai/dify/1.0.0
+cd /data/yunxinai/ragflow/0.17.0
 
 git clone https://gitcode.com/yunxinai/rag-file.git
 
-cd /data/yunxinai/dify/1.0.0/rag-file/dify/1.0.0/dify/docker
-
-cp .env.example .env
+cd /data/yunxinai/ragflow/0.17.0/rag-file/ragflow/0.17.0/dify/docker
 
 docker-compose up -d
 
-echo 'dify 1.0.0 安装完成'
+echo 'ragflow 0.17.0 安装完成'
 
 
 
